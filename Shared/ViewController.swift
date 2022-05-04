6fd57a5a384f26461ea50b82ba880754d37233ad
Anatoliy <tolxpams@gmail.com>
2022-05-04 11:25:52 +0300

    Initial Commit

---
+++ Shared/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shared
+//
+//  Created by Anatoliy Khramchenko on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
